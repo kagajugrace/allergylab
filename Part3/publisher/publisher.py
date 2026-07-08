59012b3e798daf956de759a6e05557e09d3e9e1e
Marie Grace KAGAJU <kagajugrace@gmail.com>
2026-07-08 13:45:55 +0200

    Add GitHub Actions Docker workflow

--- Part3/publisher/publisher.py
+++ Part3/publisher/publisher.py
@@ -2,4 +2,8 @@ import redis
 
 r = redis.Redis(host='redis-server', port=6379, db=0)
 
-r.publish('food_info', 'Ham Sandwich')
+r.publish('food_info', 'tuna Sandwich')
+
+r.publish('food_info', 'Orange')
+
+r.publish('food_info', 'Chocolate Cake')
